b6afbe786225d6a580ad81fc836202fcbd9fe520
FujiyamaMasatoshi <fujiyama.masatoshi.130426@gmail.com>
2024-08-19 23:14:37 +0900

    update Animal and Game

--- Assets/Scripts/Model/Game.cs
+++ Assets/Scripts/Model/Game.cs
@@ -4,15 +4,80 @@ using UnityEngine;
 
 public class Game : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField, Header("全てのAnimalの数")] private List<Animal> animals;
+    [SerializeField, Header("キメラを生成する数")] private int numChimera = 5;
+
+    
+    public List<Animal> gameUsedAnimals = new List<Animal>(); // ゲームで使用されるAnimalsを保持するリスト
+    public List<string> answerChimeraNames = new List<string>(); // ゲームの答えとして用意されたChimeraの名前
+
+    private void Start()
     {
-        
+        InitGame();
     }
 
-    // Update is called once per frame
-    void Update()
+    // ゲーム開始時に呼ぶ初期化メソッド
+    public void InitGame()
     {
-        
+        // ゲーム情報の初期化
+        gameUsedAnimals.Clear();
+        answerChimeraNames.Clear();
+
+        // ゲーム情報の生成
+        SelectRandomAnimals();
+        // 答えの生成
+        MakeAnswer();
+
     }
+
+
+    // numChimera*2の数だけゲームで使用するAnimalのリストに追加する
+    private void SelectRandomAnimals()
+    {
+        // アニマルIDから、ランダムにAnimalを選択するtempListを生成
+        List<int> animalID = new List<int>();
+        for (int i = 0; i<animals.Count; i++) animalID.Add(i);
+
+        // numChimeraの数だけ、ランダムにAnimalをgameUsedAnimalsにセットしていく
+        for (int i = 0; i<numChimera*2; i++)
+        {
+            int random = Random.Range(0, animalID.Count);
+            gameUsedAnimals.Add(animals[random]);
+            animalID.RemoveAt(random);
+        }
+    }
+
+    // 答えとなる組み合わせを生成する
+    private void MakeAnswer()
+    {
+        // gameUsedAnimalsから、2種類を選択して、generatedChimeraNameに追加する
+
+        // とりあえずは、ランダムに生成する
+        for (int i=0; i<numChimera; i++)
+        {
+            // FirstCharacterを取得するAnimal
+            int randomFirst = Random.Range(0, gameUsedAnimals.Count);
+            Animal firstAnimal = gameUsedAnimals[randomFirst];
+            
+
+            // LastCharacterを取得するAnimal
+            int randomLast = Random.Range(0, gameUsedAnimals.Count);
+            Animal lastAnimal = gameUsedAnimals[randomLast];
+            
+
+            // {first, last} Animalから文字を取得してanswerChimeraNamesに追加する
+            string firstChar = firstAnimal.GetFirstCharacter().ToString();
+            string lastChar = lastAnimal.GetLastCharacter().ToString();
+
+            // answerChimeraNamesに解を追加
+            answerChimeraNames.Add(firstChar + lastChar);
+
+            // Answer生成に使ったAnimalはリストから削除
+            gameUsedAnimals.RemoveAt(randomFirst);
+            gameUsedAnimals.RemoveAt(randomLast);
+        }
+    }
+
+
+
 }
