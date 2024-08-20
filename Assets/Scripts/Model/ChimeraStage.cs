02ec34d4fae730fe529fb88009aea558895617b3
yewt55 <moriyu5555@gmail.com>
2024-08-20 17:05:36 +0900

    make class ChimeraStage and add Select to Animal Prefabs

---
+++ Assets/Scripts/Model/ChimeraStage.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChimeraStage : MonoBehaviour
+{
+    [SerializeField] private Box leftBox = null;
+    [SerializeField] private Box rightBox = null;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
