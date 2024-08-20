44798db76aea3a6801a7901da81c67fbfef4a25e
RyoyaMaejima <ryoyam0528@gmail.com>
2024-08-20 12:09:43 +0900

    Box実装

---
+++ Assets/Scripts/Model/Box.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour
+{
+    [Header("左のボックスか")] public bool isFirstBox;
+    private Game game = null;
+    private Animal animal = null;
+    private string animalTag = "Animal";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // コンポーネントのインスタンスを捕まえる
+        game = transform.parent.GetComponent<Game>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // 動物との衝突判定
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == animalTag)
+        {
+            animal = collision.gameObject.GetComponent<Animal>();
+            if (isFirstBox)
+            {
+                game.SetFirstAnimal(animal);
+            }
+            else
+            {
+                game.SetLastAnimal(animal);
+            }
+        }
+    }
+}
