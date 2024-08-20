5e152519ec8ca69ec9783ebaa61f56eb3adb02e3
RyoyaMaejima <ryoyam0528@gmail.com>
2024-08-20 19:57:05 +0900

    ドラッグアンドドロップ修正

--- Assets/Scripts/Model/Box.cs
+++ Assets/Scripts/Model/Box.cs
@@ -7,7 +7,9 @@ public class Box : MonoBehaviour
     [Header("左のボックスかどうか")] public bool isFirstBox;
     [SerializeField] private Game game = null;
     private Animal animal = null;
+    private Animal inBoxAnimal = null;
     private Select select = null;
+    private bool isSet = false;  // セット可能か
     private string animalTag = "Animal";
 
     // Start is called before the first frame update
@@ -33,7 +35,14 @@ public class Box : MonoBehaviour
         animal = collision.gameObject.GetComponent<Animal>();
         select = collision.gameObject.GetComponent<Select>();
 
-        if (!select.GetIsDrag())
+        if (inBoxAnimal == null)
+        {
+            inBoxAnimal = animal;
+            isSet = true;
+            select.SetIsSet(true);
+        }
+
+        if (isSet && !select.GetIsDrag())
         {
             if (isFirstBox)
             {
@@ -43,6 +52,35 @@ public class Box : MonoBehaviour
             {
                 game.SetLastAnimal(animal);
             }
+
+            isSet = false;
+        }
+    }
+
+    // 動物の脱出時の処理
+    void AnimalExit(Collider2D collision)
+    {
+        if (game == null)
+        {
+            return;
+        }
+
+        animal = collision.gameObject.GetComponent<Animal>();
+        select = collision.gameObject.GetComponent<Select>();
+
+        if (animal == inBoxAnimal)
+        {
+            if (isFirstBox)
+            {
+                game.SetFirstAnimal(null);
+            }
+            else
+            {
+                game.SetLastAnimal(null);
+            }
+
+            inBoxAnimal = null;
+            select.SetIsSet(false);
         }
     }
 
@@ -65,15 +103,11 @@ public class Box : MonoBehaviour
     }
 
     // 動物の脱出判定
-    private void OnTriggerExit2D(Collider2D collsion)
+    private void OnTriggerExit2D(Collider2D collision)
     {
-        if (isFirstBox)
+        if (collision.tag == animalTag)
         {
-            game.SetFirstAnimal(null);
-        }
-        else
-        {
-            game.SetLastAnimal(null);
+            AnimalExit(collision);
         }
     }
 }
